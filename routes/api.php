b2f5679b37891724fd1ba4a1cfd1dfa0a94b7d9d
SamuelCostaDev <dev.samuelcosta@gmail.com>
2024-11-16 18:38:32 -0300

    desafio-api-laravel-dio

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloWorldController;
+use App\Http\Controllers\BandController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::get('hello/{name}', function ($name) {
+//     return 'hello ' . $name;
+// });
+// Route::post('hello-post/{name}',  [HelloWorldController::class, 'hello']);
+
+Route::get('bands', [BandController::class, 'getAll']);
+Route::post('bands/store', [BandController::class, 'store']);
+Route::get('bands/gender/{gender}', 'BandController@getBandsByGender');
+Route::get('bands/{id}', [BandController::class, 'getById']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
